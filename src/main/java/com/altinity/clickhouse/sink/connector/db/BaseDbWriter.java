261f439bd72544dcac8c81ffd89971b84a458ab8
Sunil Kumar Saini <sunil.saini@setu.co>
2023-02-13 17:14:43 +0530

    auto create db

--- src/main/java/com/altinity/clickhouse/sink/connector/db/BaseDbWriter.java
+++ src/main/java/com/altinity/clickhouse/sink/connector/db/BaseDbWriter.java
@@ -1,6 +1,7 @@
 package com.altinity.clickhouse.sink.connector.db;
 
 import com.altinity.clickhouse.sink.connector.ClickHouseSinkConnectorConfig;
+import com.altinity.clickhouse.sink.connector.db.operations.ClickHouseCreateDatabase;
 import com.clickhouse.jdbc.ClickHouseConnection;
 import com.clickhouse.jdbc.ClickHouseDataSource;
 
@@ -33,6 +34,7 @@ public class BaseDbWriter {
     ) {
         // split hostname with commas
         String[] hostNameStrings = hostNames.split(",",0);
+        tryCreatingDB(hostNameStrings,port,database,userName,password,enableSsl);
         for (int i=0; i<hostNameStrings.length; i++){
             String connectionUrl = getConnectionString((String)hostNameStrings[i], port, database, enableSsl);
             this.createConnection(connectionUrl, "Agent_" + i, userName, password);
@@ -47,6 +49,37 @@ public class BaseDbWriter {
             return String.format("jdbc:clickhouse://%s:%s/%s?ssl=true", hostName, port, database);
         }
         return String.format("jdbc:clickhouse://%s:%s/%s", hostName, port, database);
+    }
+
+    public void tryCreatingDB(String[] hostNameStrings,Integer port,String database,String userName,String password,Boolean enableSsl){
+        for (int i=0; i<hostNameStrings.length; i++){
+            ClickHouseConnection conn;
+            try {
+                String connectionUrl = getConnectionStringWithOutDB((String)hostNameStrings[i], port, enableSsl);
+                Properties properties = new Properties();
+                properties.setProperty("client_name", "DB_CreateAgent");
+                properties.setProperty("custom_settings", "allow_experimental_object_type=1");
+                ClickHouseDataSource dataSource = new ClickHouseDataSource(connectionUrl, properties);
+                conn = dataSource.getConnection(userName, password);
+                DBMetadata metadata = new DBMetadata();
+                if(false == metadata.checkIfDatabaseExists(conn, database)) {
+                    try {
+                        new ClickHouseCreateDatabase().createNewDatabase(conn, database);
+                    } catch (Exception e) {
+                        log.error("Error creating ClickHouse database" + e);
+                    }
+                }
+            } catch (Exception e) {
+                log.error("Error creating ClickHouse connection" + e);
+            }
+        }
+    }
+
+    public String getConnectionStringWithOutDB(String hostName, Integer port, Boolean enableSsl) {
+        if (enableSsl){
+            return String.format("jdbc:clickhouse://%s:%s?ssl=true", hostName, port);
+        }
+        return String.format("jdbc:clickhouse://%s:%s", hostName, port);
     }
 
     /**
